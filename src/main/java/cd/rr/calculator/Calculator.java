323a0db217809152fcb02e581267a58b9a1f6103
DeShawn Wallace <deshawnwallace@DeShawns-MacBook-Air.local>
2021-07-29 14:13:01 -0400

    Calculations

--- src/main/java/cd/rr/calculator/Calculator.java
+++ src/main/java/cd/rr/calculator/Calculator.java
@@ -3,5 +3,44 @@ package cd.rr.calculator;
 public class Calculator {
     public static void main(String[] args) {
         // Your solution here!
+        int firstNumber, secondNumber;
+        public static void addition(int firstNumber, int secondNumber){
+            System.out.println(firstNumber + secondNumber);
+        }
+
+        public static void subtraction(int firstNumber, int secondNumber){
+            System.out.println(firstNumber - secondNumber);
+        }
+
+        public static void multiplication(int firstNumber, int secondNumber){
+            System.out.println(firstNumber * secondNumber);
+        }
+
+        public static void division(int firstNumber, int secondNumber){
+            System.out.println(String.format("%2.f", firstNumber / secondNumber));
+        }
+
+
+    switch(op){
+        case "+" :
+            result =  +  ;
+            System.out.println("Your result: " + result);
+            break;
+        case "-" :
+            result =  -  ;
+            System.out.println("Your result: " + result);
+            break;
+        case "*" :
+            result =  *  ;
+            System.out.println("Your result: " + result);
+            break;
+        case "/" :
+            result =  /  ;
+            System.out.println("Your result: " + result);
+            break;
+        default:
+            System.out.println("Exit!");
+            break;
+    }
     }
 }
