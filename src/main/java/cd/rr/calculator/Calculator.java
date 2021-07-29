5488685907cab3126c55a551a2ad054b2682b389
JonKappa <cappytwoj@gmail.com>
2021-07-29 14:30:41 -0400

    'calculatormenu'

--- src/main/java/cd/rr/calculator/Calculator.java
+++ src/main/java/cd/rr/calculator/Calculator.java
@@ -1,7 +1,50 @@
 package cd.rr.calculator;
 
+import java.util.Scanner;
+
+
 public class Calculator {
+
+
+    public static void Menu(){
+        System.out.println("Welcome to the CLI Calculator");
+        System.out.println("-----------------------------\n");
+        System.out.println("Opperations: ");
+        System.out.println("""
+                1: Addition
+                2: Subtraction
+                3: Multiplication
+                4: Division
+                5: Exit""");
+        System.out.print("Please choose an operation (1-5): ");
+
+    }
     public static void main(String[] args) {
-        // Your solution here!
+        Scanner input = new Scanner(System.in);
+        Calculator.Menu();
+        int userInput = input.nextInt();
+        do {
+            switch (userInput) {
+                case 1:
+                    System.out.print("Enter two numbers: ");
+                    int num1 = input.nextInt();
+                    int num2 = input.nextInt();
+                    Calculator.Menu();
+                    userInput = input.nextInt();
+                    break;
+
+                case 2:
+
+                case 3:
+
+                case 4:
+
+            }
+        }while (userInput != 5);
+        if (userInput == 5) {
+            System.out.println("Goodbye!");
+            System.exit(0);
+        }
+
     }
 }
