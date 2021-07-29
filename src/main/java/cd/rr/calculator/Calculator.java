5a879b3ee3db198ccf71f7f7b1411880ef594009
Adonis-Cuello <adonisac2002@gmail.com>
2021-07-29 14:09:53 -0400

    errors and limitations

--- src/main/java/cd/rr/calculator/Calculator.java
+++ src/main/java/cd/rr/calculator/Calculator.java
@@ -3,5 +3,9 @@ package cd.rr.calculator;
 public class Calculator {
     public static void main(String[] args) {
         // Your solution here!
+//    if(userinput == "/" && num1 == 0 || num2 == 0){
+//        System.out.println("Error");
+//    }
+//        System.out.println(String.format("%.2f" ,divide));
     }
 }
