336d518326452b1c4828babec7e117209fed560f
Adonis-Cuello <adonisac2002@gmail.com>
2021-07-31 15:12:55 -0400

    final after review pt2

--- src/main/java/cd/rr/calculator/Main.java
+++ src/main/java/cd/rr/calculator/Main.java
@@ -10,51 +10,58 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        Calculator.Menu();
-        int userInput = input.nextInt();
-        do {
-            System.out.print("Enter one number: ");
-            int inNum1 = input.nextInt();
-            System.out.println("Enter another number: ");
-            int inNum2 = input.nextInt();
 
+        int userInput;
+        int inNum1;
+        int inNum2;
+        do {
+            Calculator.Menu();
+            userInput = input.nextInt();
             switch (userInput) {
                 case 1:
+                    System.out.print("Enter one number: ");
+                    inNum1 = input.nextInt();
+                    System.out.print("Enter another number: ");
+                    inNum2 = input.nextInt();
                     Calculator.addition(inNum1, inNum2);
-                    Calculator.Menu();
-                    userInput = input.nextInt();
                     break;
 
                 case 2:
+                    System.out.print("Enter one number: ");
+                    inNum1 = input.nextInt();
+                    System.out.print("Enter another number: ");
+                    inNum2 = input.nextInt();
                     Calculator.subtraction(inNum1, inNum2);
-                    Calculator.Menu();
-                    userInput = input.nextInt();
                     break;
 
                 case 3:
+                    System.out.print("Enter one number: ");
+                    inNum1 = input.nextInt();
+                    System.out.print("Enter another number: ");
+                    inNum2 = input.nextInt();
                     Calculator.multiplication(inNum1, inNum2);
-                    Calculator.Menu();
-                    userInput = input.nextInt();
                     break;
 
                 case 4:
-                    if(userInput == 4 && inNum2 == 0){
+                    System.out.print("Enter one number: ");
+                    inNum1 = input.nextInt();
+                    System.out.print("Enter another number: ");
+                    inNum2 = input.nextInt();
+                    if(inNum2 == 0){
                         System.out.println("Error: Cannot divide by 0");
-                        Calculator.Menu();
-                        userInput = input.nextInt();
-                        break;
+
                     }
                     else {
                         Calculator.division(inNum1, inNum2);
-                        Calculator.Menu();
-                        userInput = input.nextInt();
-                        break;
                     }
+                    break;
+                default:
+                    System.out.println("Not a valid input, try again.");
             }
         }while (userInput != 5);
-        if (userInput == 5) {
+
             System.out.println("Goodbye!");
             System.exit(0);
         }
     }
-}
+
